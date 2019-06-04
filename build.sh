02f6fe6664e2bca8f6e612e3ecac408f5f331df0
prasannatumu <39209817+prasannatumu@users.noreply.github.com>
2019-06-04 14:45:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo docker build -t webapp:v1 ./website/
-sudo docker run -it -p 3001:80 --name webcontainer webapp:v1 
+sudo docker build -t webapp:latest ./website/
+sudo docker run -it -p 3001:80 webapp:latest
